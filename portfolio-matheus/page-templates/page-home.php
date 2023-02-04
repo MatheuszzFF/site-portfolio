23328ba055bc53394023fa500b34995d92598550
mzmfelix <matheus.flaviano@mzgroup.com>
2023-02-04 17:23:03 -0300

    refactor(home banner) change the self image to array field on acf

--- portfolio-matheus/page-templates/page-home.php
+++ portfolio-matheus/page-templates/page-home.php
@@ -5,7 +5,17 @@
             <div class="bannerBox">
                 <div class="d-flex">
                     <h2><?php the_field('banner_left_text') ?></h2>
-                    <img src="<?php the_field('banner_image_self') ?>" alt="">
+                    <?php 
+                        $image = get_sub_field('home_asset_image');
+                        $image_url = $image['url'];
+                        $image_alt = $image['alt'];
+                    
+                        if( !empty( $image ) ) {
+                            echo <<<IMG
+                            <img src="$image_url" alt="$image_alt" />
+                            IMG;
+                        } 
+                    ?>
                     <h2><?php the_field('banner_right_image') ?></h2>
                 </div>
             </div>

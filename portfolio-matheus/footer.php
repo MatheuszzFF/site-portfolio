accaf6ce93e9c0ee0919b17bf4b038359fef70d2
mzmfelix <matheus.flaviano@mzgroup.com>
2023-02-04 14:35:13 -0300

    build(global) create major files for the theme

--- portfolio-matheus/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-    <footer class='footer'>
-        <div class="container">
-            <p>Todos os direitos Reservados</p>
-        </div>
-    </footer>
-    
-    <?php wp_footer(); ?>
-<body>
--- portfolio-matheus/partials/includes/add_css_jss.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    function register_portfolio_script_style() {
-        //JS
-        wp_enqueue_script('main',  get_template_directory_uri() . "/js/main.js", '', '' , true );
-    }
-
-    add_action('wp_enqueue_scripts', 'register_portfolio_script_style');

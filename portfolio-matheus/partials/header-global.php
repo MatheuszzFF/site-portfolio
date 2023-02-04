37f3ceb69e4c6350ee74ef1f94c6e19e6198c424
mzmfelix <matheus.flaviano@mzgroup.com>
2023-02-04 17:00:33 -0300

    refactor(logo) change the logo format for normal html

--- portfolio-matheus/partials/header-global.php
+++ portfolio-matheus/partials/header-global.php
@@ -3,13 +3,7 @@
    <div class="container">
         <div class="d-flex align-items-center justify-content-between">
             <div class="header__logo">
-                <?php 
-                    $link = get_field('logo');
-                    $link_url = $link['url'];
-                    $link_title = $link['title'];
-                    $link_target = $link['target'] ? $link['target'] : '_self';
-                ?>
-                <a href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                <a href="<?php get_home_url(); ?>">M</a>
             </div>
     
             <nav class="header__menu">

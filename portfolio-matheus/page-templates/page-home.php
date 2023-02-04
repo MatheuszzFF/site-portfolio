3db6bbb92dd517bfd0235c6576e7dc5b98d9daed
mzmfelix <matheus.flaviano@mzgroup.com>
2023-02-04 17:46:20 -0300

    fix(acf) fixing field names

--- portfolio-matheus/page-templates/page-home.php
+++ portfolio-matheus/page-templates/page-home.php
@@ -8,7 +8,7 @@
                 <div class="d-flex">
                     <h2><?php the_field('banner_left_text') ?></h2>
                     <?php 
-                        $image = get_sub_field('home_asset_image');
+                        $image = get_sub_field('banner_self_image');
                         $image_url = $image['url'];
                         $image_alt = $image['alt'];
                     
@@ -18,7 +18,7 @@
                             IMG;
                         } 
                     ?>
-                    <h2><?php the_field('banner_right_image') ?></h2>
+                    <h2><?php the_field('banner_right_text') ?></h2>
                 </div>
             </div>
         </section>

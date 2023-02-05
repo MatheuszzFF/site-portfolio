3f95264493b2a04d452cf35cd706a52ab0a998a0
mzmfelix <matheus.flaviano@mzgroup.com>
2023-02-05 14:01:52 -0300

    build(global) add dependences for main.js, mobile btn function add in main.js

--- portfolio-matheus/includes/add_css_js.php
+++ portfolio-matheus/includes/add_css_js.php
@@ -1,6 +1,6 @@
 <?php 
     function portfolio_register_scripts() {
-         wp_register_script('main',  get_template_directory_uri() . "js/main.js", '', true, '' );
+         wp_register_script('main',  get_template_directory_uri() . "/js/main.js", '', true, '' );
         wp_enqueue_script('main');
     }
 

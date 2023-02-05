076334149daf613005bb05233dc63929f36002e2
mzmfelix <matheus.flaviano@mzgroup.com>
2023-02-05 14:06:04 -0300

    test(mobile header) testing mobileBtnListener function

--- portfolio-matheus/js/main.js
+++ portfolio-matheus/js/main.js
@@ -1,6 +1,8 @@
 function mobileBtnListener() {
     let btn_el = document.querySelector('.header__mobileBtn');
     let nav_el = document.querySelector('.header__menu');
+    console.log(btn_el);
+    console.log(nav_el);
     
     btn_el.addEventListener('click', () => {
         btn_el.classList.toggle('active');

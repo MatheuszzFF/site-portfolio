d344011efff0fee4bed00dff8762350d26681eed
mzmfelix <matheus.flaviano@mzgroup.com>
2023-02-05 14:09:20 -0300

    test(mobile header) testing mobileBtnListener function

--- portfolio-matheus/js/main.js
+++ portfolio-matheus/js/main.js
@@ -1,13 +1,15 @@
-function mobileBtnListener() {
-    let btn_el = document.querySelector('.header__mobileBtn');
-    let nav_el = document.querySelector('.header__menu');
-    console.log(btn_el);
-    console.log(nav_el);
+window.addEventListener('load' , () => {
+    function mobileBtnListener() {
+        let btn_el = document.querySelector('.header__mobileBtn');
+        let nav_el = document.querySelector('.header__menu');
+        console.log(btn_el);
+        console.log(nav_el);
+        
+        btn_el.addEventListener('click', () => {
+            btn_el.classList.toggle('active');
+            nav_el.classList.toggle('active');
+        })
+    }
     
-    btn_el.addEventListener('click', () => {
-        btn_el.classList.toggle('active');
-        nav_el.classList.toggle('active');
-    })
-}
-
-mobileBtnListener();
+    mobileBtnListener();
+})
